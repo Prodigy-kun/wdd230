b4078e374b45683e82e6930a1a978c9b41ca3d11
prodigy_kun <ilerioluwaomotayo@gmail.com>
2022-10-31 00:38:38 +0100

    discover page

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -19,3 +19,38 @@ function toggleMenu() {
 }
 const x = document.getElementById("ham");
 x.onclick = toggleMenu
+
+// local storage
+const visit = document.querySelector('.visit')
+// const times = window.localStorage.setItem('visit', '0')
+let update = window.localStorage.getItem('visit')
+if (Number(update) === 0) {
+    visit.textContent = 'This is your first time'
+} else {
+    visit.textContent = update
+}
+update++
+localStorage.setItem('visit', update)
+//Lazyloading
+let imagesToLoad = document.querySelectorAll('img[data-src]')
+const loadImages = (images) => {
+    images.setAttribute('src', images.getAttribute('data-src'))
+    images.onload = () => images.removeAttribute('data-src')
+}
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target)
+            }
+        })
+    })
+    imagesToLoad.forEach((img) => {
+        observer.observe(img)
+    })
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img)
+    })
+}
